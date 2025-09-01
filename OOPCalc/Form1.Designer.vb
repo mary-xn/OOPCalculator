a3d30f91b5c844a58350ddfbcf5e7132b2cf5d98
Mary Sembrero <sembrero.maryjoybscs2024@gmail.com>
2025-09-01 22:36:10 +0800

    Add project files.

---
+++ OOPCalc/Form1.Designer.vb
@@ -0,0 +1,290 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btn1 = New Button()
+        btn2 = New Button()
+        btn3 = New Button()
+        btnAdd = New Button()
+        btnSubtract = New Button()
+        btn6 = New Button()
+        btn5 = New Button()
+        btn4 = New Button()
+        btnMultiply = New Button()
+        btn9 = New Button()
+        btn8 = New Button()
+        btn7 = New Button()
+        btnDivide = New Button()
+        btnClear = New Button()
+        btnDecimal = New Button()
+        btn0 = New Button()
+        Button17 = New Button()
+        txtbox1 = New TextBox()
+        txtbox2 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btn1
+        ' 
+        btn1.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn1.Location = New Point(12, 121)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(75, 65)
+        btn1.TabIndex = 0
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn2.Location = New Point(93, 121)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(75, 65)
+        btn2.TabIndex = 1
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btn3
+        ' 
+        btn3.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn3.Location = New Point(174, 121)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(75, 65)
+        btn3.TabIndex = 2
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnAdd.Location = New Point(255, 121)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(75, 65)
+        btnAdd.TabIndex = 3
+        btnAdd.Text = "+"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnSubtract
+        ' 
+        btnSubtract.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnSubtract.Location = New Point(252, 209)
+        btnSubtract.Name = "btnSubtract"
+        btnSubtract.Size = New Size(75, 65)
+        btnSubtract.TabIndex = 7
+        btnSubtract.Text = "-"
+        btnSubtract.UseVisualStyleBackColor = True
+        ' 
+        ' btn6
+        ' 
+        btn6.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn6.Location = New Point(171, 209)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(75, 65)
+        btn6.TabIndex = 6
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn5.Location = New Point(90, 209)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(75, 65)
+        btn5.TabIndex = 5
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn4.Location = New Point(9, 209)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(75, 65)
+        btn4.TabIndex = 4
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btnMultiply
+        ' 
+        btnMultiply.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnMultiply.Location = New Point(255, 295)
+        btnMultiply.Name = "btnMultiply"
+        btnMultiply.Size = New Size(75, 65)
+        btnMultiply.TabIndex = 11
+        btnMultiply.Text = "*"
+        btnMultiply.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn9.Location = New Point(174, 295)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(75, 65)
+        btn9.TabIndex = 10
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btn8
+        ' 
+        btn8.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn8.Location = New Point(93, 295)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(75, 65)
+        btn8.TabIndex = 9
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn7.Location = New Point(12, 295)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(75, 65)
+        btn7.TabIndex = 8
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' btnDivide
+        ' 
+        btnDivide.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnDivide.Location = New Point(255, 381)
+        btnDivide.Name = "btnDivide"
+        btnDivide.Size = New Size(75, 65)
+        btnDivide.TabIndex = 15
+        btnDivide.Text = "/"
+        btnDivide.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnClear.Location = New Point(174, 381)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(75, 65)
+        btnClear.TabIndex = 14
+        btnClear.Text = "C"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnDecimal
+        ' 
+        btnDecimal.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnDecimal.Location = New Point(93, 381)
+        btnDecimal.Name = "btnDecimal"
+        btnDecimal.Size = New Size(75, 65)
+        btnDecimal.TabIndex = 13
+        btnDecimal.Text = "."
+        btnDecimal.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn0.Location = New Point(12, 381)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(75, 65)
+        btn0.TabIndex = 12
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' Button17
+        ' 
+        Button17.Font = New Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button17.Location = New Point(12, 452)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(315, 65)
+        Button17.TabIndex = 19
+        Button17.Text = "="
+        Button17.UseVisualStyleBackColor = True
+        ' 
+        ' txtbox1
+        ' 
+        txtbox1.BorderStyle = BorderStyle.None
+        txtbox1.Font = New Font("Times New Roman", 36F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        txtbox1.HideSelection = False
+        txtbox1.Location = New Point(12, 44)
+        txtbox1.Multiline = True
+        txtbox1.Name = "txtbox1"
+        txtbox1.Size = New Size(315, 59)
+        txtbox1.TabIndex = 20
+        txtbox1.Text = "0"
+        txtbox1.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' txtbox2
+        ' 
+        txtbox2.BorderStyle = BorderStyle.None
+        txtbox2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtbox2.Location = New Point(9, 12)
+        txtbox2.Multiline = True
+        txtbox2.Name = "txtbox2"
+        txtbox2.Size = New Size(318, 33)
+        txtbox2.TabIndex = 21
+        txtbox2.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(339, 537)
+        Controls.Add(txtbox2)
+        Controls.Add(txtbox1)
+        Controls.Add(Button17)
+        Controls.Add(btnDivide)
+        Controls.Add(btnClear)
+        Controls.Add(btnDecimal)
+        Controls.Add(btn0)
+        Controls.Add(btnMultiply)
+        Controls.Add(btn9)
+        Controls.Add(btn8)
+        Controls.Add(btn7)
+        Controls.Add(btnSubtract)
+        Controls.Add(btn6)
+        Controls.Add(btn5)
+        Controls.Add(btn4)
+        Controls.Add(btnAdd)
+        Controls.Add(btn3)
+        Controls.Add(btn2)
+        Controls.Add(btn1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnDecimal As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents txtbox1 As TextBox
+    Friend WithEvents txtbox2 As TextBox
+
+End Class
---
+++ OOPCalc/Form1.vb
@@ -0,0 +1,154 @@
+﻿Public Class Form1
+
+    Dim FirstNum As Decimal
+    Dim SecondNum As Decimal
+    Dim OperationNumber As Integer
+    Dim OperationSign As String
+    Dim OperatorSelector As Boolean = False
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        If txtbox1.Text <> "0" Then
+            txtbox1.Text += "1"
+        Else
+            txtbox1.Text = "1"
+        End If
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        If txtbox1.Text <> "0" Then
+            txtbox1.Text += "2"
+        Else
+            txtbox1.Text = "2"
+        End If
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        If txtbox1.Text <> "0" Then
+            txtbox1.Text += "3"
+        Else
+            txtbox1.Text = "3"
+        End If
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        If txtbox1.Text <> "0" Then
+            txtbox1.Text += "4"
+        Else
+            txtbox1.Text = "4"
+        End If
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        If txtbox1.Text <> "0" Then
+            txtbox1.Text += "5"
+        Else
+            txtbox1.Text = "5"
+        End If
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        If txtbox1.Text <> "0" Then
+            txtbox1.Text += "6"
+        Else
+            txtbox1.Text = "6"
+        End If
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        If txtbox1.Text <> "0" Then
+            txtbox1.Text += "7"
+        Else
+            txtbox1.Text = "7"
+        End If
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        If txtbox1.Text <> "0" Then
+            txtbox1.Text += "8"
+        Else
+            txtbox1.Text = "8"
+        End If
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        If txtbox1.Text <> "0" Then
+            txtbox1.Text += "9"
+        Else
+            txtbox1.Text = "9"
+        End If
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        If txtbox1.Text <> "0" Then
+            txtbox1.Text += "0"
+        End If
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        FirstNum = txtbox1.Text
+        OperationSign = "+"
+        txtbox2.Text = FirstNum & " " & OperationSign & " "
+        txtbox1.Text = "0"
+        OperatorSelector = True
+        OperationNumber = 1 '=+
+    End Sub
+
+    Private Sub btnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+        FirstNum = txtbox1.Text
+        OperationSign = "-"
+        txtbox2.Text = FirstNum & " " & OperationSign & " "
+        txtbox1.Text = "0"
+        OperatorSelector = True
+        OperationNumber = 2 '=-
+    End Sub
+
+    Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+        FirstNum = txtbox1.Text
+        OperationSign = "*"
+        txtbox2.Text = FirstNum & " " & OperationSign & " "
+        txtbox1.Text = "0"
+        OperatorSelector = True
+        OperationNumber = 3 '=*
+    End Sub
+
+    Private Sub btnDivide_Click(sender As Object, e As EventArgs) Handles btnDivide.Click
+        FirstNum = txtbox1.Text
+        OperationSign = "/"
+        txtbox2.Text = FirstNum & " " & OperationSign & " "
+        txtbox1.Text = "0"
+        OperatorSelector = True
+        OperationNumber = 4 '=/
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        If OperatorSelector = True Then
+            SecondNum = txtbox1.Text
+
+            If OperationNumber = 1 Then
+                txtbox1.Text = FirstNum + SecondNum
+            ElseIf OperationNumber = 2 Then
+                txtbox1.Text = FirstNum - SecondNum
+            ElseIf OperationNumber = 3 Then
+                txtbox1.Text = FirstNum * SecondNum
+            Else
+                If SecondNum = 0 Then
+                    txtbox1.Text = "Error!"
+                Else
+                    txtbox1.Text = FirstNum / SecondNum
+                End If
+            End If
+            txtbox2.Text = FirstNum & " " & OperationSign & " " & SecondNum & " = "
+            OperatorSelector = False
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtbox1.Text = "0"
+        txtbox2.Text = ""
+    End Sub
+
+    Private Sub txtbox2_TextChanged(sender As Object, e As EventArgs) Handles txtbox2.TextChanged
+
+    End Sub
+
+End Class
